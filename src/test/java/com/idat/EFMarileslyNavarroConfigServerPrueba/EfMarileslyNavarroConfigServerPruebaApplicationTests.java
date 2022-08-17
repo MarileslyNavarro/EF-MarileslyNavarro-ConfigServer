c923eaea10e31c45183982e9cc61a5378a7f705a
Marilesly Navarro <jhazmynnavarro@gmail.com>
2022-08-17 00:55:02 -0500

    Config Server

---
+++ src/test/java/com/idat/EFMarileslyNavarroConfigServerPrueba/EfMarileslyNavarroConfigServerPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFMarileslyNavarroConfigServerPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfMarileslyNavarroConfigServerPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
